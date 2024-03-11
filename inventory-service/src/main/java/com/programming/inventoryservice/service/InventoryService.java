a074953b5f4dfbd7a8667b8068366bf585df8f6b
SivaGanesh <sivaganeshp4@gmail.com>
2024-03-11 09:42:00 -0400

    shopping cart services

---
+++ inventory-service/src/main/java/com/programming/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.programming.inventoryservice.service;
+
+import com.programming.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
